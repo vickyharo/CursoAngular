0a2f1994f734321e6f7bf7679517da5ebe27c502
Victoria Haro <vickyharo2805@gmail.com>
2025-02-24 11:26:58 -0600

    proyecto010

---
+++ proyecto010/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'proyecto010';
+
+  articulos: any;
+  naturalezas: any;
+
+  constructor(private http: HttpClient) { }
+
+
+  ngOnInit() {
+    //De bansi
+    this.http.get("https://localhost:7152/api/ModificacionCuenta/ObtenerNaturalezas")
+    .subscribe(
+      resultado => {
+        this.naturalezas = resultado;
+      }
+    );
+
+    //Ejemplo
+   /* this.http.get("https://ejerciciostutorialesya.com/vue/datos.php")
+      .subscribe(
+        resultado => {
+          this.articulos = resultado;
+        }
+      );*/
+
+      console.log(this.naturalezas);
+  }
+}
